4c66fba34d769c05b3528efa9e1bb94cddf863d0
Paurakh1 <paurakhsaud2022@gmail.com>
2023-02-26 08:04:19 +0545

    adding dashboard to app

---
+++ app/src/main/java/com/example/myapplication/dashboard/Dashboard.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+    }
+}
