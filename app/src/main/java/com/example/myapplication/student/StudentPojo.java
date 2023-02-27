5fa359dd0e83f1d660f7349da2d8c81f270505a0
abnormalbbk <abnormal.bbk@gmail.com>
2023-02-27 09:00:19 +0545

    :sparkles: Created StudentPojo

---
+++ app/src/main/java/com/example/myapplication/student/StudentPojo.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.student;
+
+public class StudentPojo {
+    final String name;
+    final int age;
+
+    public StudentPojo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
