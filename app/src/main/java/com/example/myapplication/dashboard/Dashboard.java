d228e51b9acad31299f36672dbc11cc727a7a435
Paurakh1 <paurakhsaud2022@gmail.com>
2023-02-28 00:14:50 +0545

    dashboard done

--- app/src/main/java/com/example/myapplication/dashboard/Dashboard.java
+++ app/src/main/java/com/example/myapplication/dashboard/Dashboard.java
@@ -5,15 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class Dashboard extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
     }
 }
