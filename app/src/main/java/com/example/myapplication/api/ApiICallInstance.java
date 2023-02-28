bf2b002dde2182728a4c017c8e5c2bc36f944894
abnormalbbk <abnormal.bbk@gmail.com>
2023-02-28 18:37:03 +0545

    :sparkles: Created instance onf Retrofit for api call

---
+++ app/src/main/java/com/example/myapplication/api/ApiICallInstance.java
@@ -0,0 +1,33 @@
+package com.example.myapplication.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiICallInstance {
+    private static Retrofit retrofit;
+//    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    private static final String BASE_URL = "https://my-json-server.typicode.com/typicode/";
+
+    /*
+        Singleton Instance of Retrofit for your app
+     */
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
+
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
