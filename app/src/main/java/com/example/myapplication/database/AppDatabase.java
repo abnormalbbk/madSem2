2595bd92efa0b2c1c78fa7e6d5b8d21f39c004f2
abnormalbbk <abnormal.bbk@gmail.com>
2023-02-28 12:00:20 +0545

    :sparkles: Added AppDatabase with singleton pattern to access database

---
+++ app/src/main/java/com/example/myapplication/database/AppDatabase.java
@@ -0,0 +1,26 @@
+package com.example.myapplication.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = User.class, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase instance;
+
+    public static synchronized AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context,
+                            AppDatabase.class,
+                            "my_app_database")
+                    .build();
+        }
+        return instance;
+    }
+
+    public abstract UserDao userDao();
+}
+
+
