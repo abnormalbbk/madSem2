fdf4f005ce4056853509f985336bbf1998b8effd
Paurakh1 <paurakhsaud2022@gmail.com>
2023-03-02 09:16:11 +0545

    I've add the fragmanet to the dashboard

--- app/src/main/java/com/example/myapplication/Dashboard.java
+++ app/src/main/java/com/example/myapplication/Dashboard.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.myapplication.R;
+import com.example.myapplication.dashboard.contact.ContactFragment;
+import com.example.myapplication.dashboard.home_page.HomeFragment;
+import com.example.myapplication.dashboard.payment.PaymentFragment;
+import com.example.myapplication.dashboard.profile.ProfileFragment;
+import com.example.myapplication.dashboard.setting.SettingFragment;
 import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Dashboard extends AppCompatActivity {
-    ActivityMainBinding binding;
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    PaymentFragment paymentFragment = new PaymentFragment();
+    ContactFragment contactFragment = new ContactFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    SettingFragment settingFragment = new SettingFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_dashboard);
 
     }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new Handler().postDelayed(() -> startActivity(new Intent(MainActivity.this,Dashboard.class)),3000);
+        finish();
+
     }
 }
